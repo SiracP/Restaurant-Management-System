188c689d1b81b0eec87cc506b12499dd23195210
Siraç Petmezçiler <75903693+SiracP@users.noreply.github.com>
2025-06-08 02:09:57 +0300

    Delete build directory

--- build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_Restaurant_Management_System_FILE E:/QT/Restaurant_Management_System/build/Desktop_Qt_6_8_1_MinGW_64_bit-Debug/Restaurant_Management_System.exe)
-set(__QT_DEPLOY_TARGET_Restaurant_Management_System_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_Restaurant_Management_System_RUNTIME_DLLS C:/Qt/6.8.1/mingw_64/bin/Qt6Charts.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6OpenGLWidgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Widgets.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.8.1/mingw_64/bin/Qt6Core.dll)
